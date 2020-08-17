c3bbe409b541cee3fd201f9e7b735c2a89e6714c
Sara A <calisaraalhindi@gmail.com>
2020-08-17 22:59:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+  
   @@count = 0
   
   attr_reader :name, :artist, :genre
@@ -7,6 +8,7 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
   end
   
   def self.count
