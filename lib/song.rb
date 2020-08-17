70bbf5f4e6c8a7c60e8e0407842222d670c848b3
Sara A <calisaraalhindi@gmail.com>
2020-08-17 23:10:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ class Song
   attr_reader :name, :artist, :genre
   
     @@count = 0
-    @@artists= []
-    @@genres= []
+    @@artists = []
+    @@genres = []
     
   def initialize(name, artist, genre)
     @name = name
